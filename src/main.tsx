65530007dfeabaeeb2354c7bc83e3fc02a35d6e9
Sadeq Mozaffari <sadeqmozaffari@gmail.com>
2024-08-02 09:15:17 +0430

    add QueryClientProvider to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import { AppProvider } from "./contexts/app/app-context.jsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
+
+import "./index.css"; // اطمینان حاصل کنید که سبک‌های شما درست وارد شده‌اند
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+   <QueryClientProvider client={queryClient}>
   <AppProvider>
     <App />
   </AppProvider>
+  </QueryClientProvider>
 );
