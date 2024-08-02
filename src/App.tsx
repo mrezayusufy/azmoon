59e309da1b5ef8683044305bcf0d7f90fadd2dc0
Sadeq Mozaffari <sadeqmozaffari@gmail.com>
2024-08-02 09:19:01 +0430

    change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import { useEffect } from "react";
 import { useAppContext } from "./contexts/app/app-context";
-
+import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 
 const App = () => {
   const { theme } = useAppContext();
@@ -17,11 +18,13 @@ const App = () => {
       head.removeChild(link);
     };
   }, [theme]);
-
+const queryClient = new QueryClient();
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <QueryClientProvider client={queryClient}>
+       <RouterProvider router={router} />
+     </QueryClientProvider>
+     
+   
   );
 };
 
